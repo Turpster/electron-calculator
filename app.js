a0ab57aeb605e60b1972411c1890a178e99b1ec1
Turpster <dicky_056@hotmail.co.uk>
2018-09-06 22:06:07 +0100

    That. Was. Easy. ✌

---
+++ app.js
@@ -0,0 +1,27 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow() {
+    win = new BrowserWindow({width: 386, height: 485})
+
+    win.loadFile("calculator.html")
+    win.setMenu(null);
+    win.on('closed', () => {
+        win = null;
+    })
+    
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin')
+    {
+        app.quit();
+    }
+})
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+})
---
+++ calculator.js
@@ -0,0 +1,84 @@
+
+var clear = false;
+
+$(document).ready(() => 
+{
+    function AddToDisplay(str)
+    {
+        if (clear)
+        {
+            $(".calculator-display").html("")
+            clear = false
+        }
+        $(".calculator-display").html($(".calculator-display").html() + str);
+    }
+
+    $(".numpad-1").click(() => 
+    {
+         AddToDisplay("1")
+    })
+    $(".numpad-2").click(() => 
+    {
+        AddToDisplay("2")
+    })
+    $(".numpad-3").click(() => 
+    {
+        AddToDisplay("3")
+    })
+    $(".numpad-4").click(() => 
+    {
+        AddToDisplay("4")
+    })
+    $(".numpad-5").click(() => 
+    {
+        AddToDisplay("5")
+    })
+    $(".numpad-6").click(() => 
+    {
+        AddToDisplay("6")
+    })
+    $(".numpad-7").click(() => 
+    {
+        AddToDisplay("7")
+    })
+    $(".numpad-8").click(() => 
+    {
+        AddToDisplay("8")
+    })
+    $(".numpad-9").click(() => 
+    {
+        AddToDisplay("9")
+    })
+    $(".numpad-0").click(() => 
+    {
+        AddToDisplay("0")
+    })
+    $(".numpad-plus").click(() => 
+    {
+        AddToDisplay("+")
+    })
+    $(".numpad-negative").click(() => 
+    {
+        AddToDisplay("-")
+    })
+    $(".numpad-multiply").click(() => 
+    {
+        AddToDisplay("*")
+    })
+    $(".numpad-divide").click(() => 
+    {
+        AddToDisplay("/")
+    })
+    $(".numpad-equals").click(() => 
+    {
+        clear = true
+        try
+        {
+            $(".calculator-display").html(math.eval($(".calculator-display").html()))
+        } 
+        catch(Exception)
+        {
+            $(".calculator-display").html("Invalid Input")    
+        }
+    })
+})
